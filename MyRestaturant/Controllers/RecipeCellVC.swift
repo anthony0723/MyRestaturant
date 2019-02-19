7a715e3b6ee6e2a79d138399e9ba544d698b1918
Anthony Baldwin <srv0723@bex.net>
2019-02-19 16:04:19 -0500

    Add Recipe tableviews Add Recipe Detail Controllers Add Recipe cell view

---
+++ MyRestaturant/Controllers/RecipeCellVC.swift
@@ -0,0 +1,24 @@
+//
+//  RecipeCellVC.swift
+//  MyRestaturant
+//
+//  Created by Anthony Baldwin on 2/19/19.
+//  Copyright © 2019 AnthonyBaldwin. All rights reserved.
+//
+
+import UIKit
+
+class RecipeCellVC: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ MyRestaturant/Controllers/RecipeDetailVC.swift
@@ -0,0 +1,30 @@
+//
+//  RecipeDetailVC.swift
+//  MyRestaturant
+//
+//  Created by Anthony Baldwin on 2/19/19.
+//  Copyright © 2019 AnthonyBaldwin. All rights reserved.
+//
+
+import UIKit
+
+class RecipeDetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
