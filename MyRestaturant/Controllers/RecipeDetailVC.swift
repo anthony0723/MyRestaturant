53d0eed92d0ff6dc2ca49b279ba6329aa1996ef3
Anthony Baldwin <srv0723@bex.net>
2019-02-22 14:01:20 -0500

    Add photo assets Add recipe detail buttons / textfields / outlets / actions Add button model

--- MyRestaturant/Controllers/RecipeDetailVC.swift
+++ MyRestaturant/Controllers/RecipeDetailVC.swift
@@ -9,14 +9,41 @@
 import UIKit
 
 class RecipeDetailVC: UIViewController {
-
+    @IBOutlet weak var recipeLabel: UILabel!
+    
+    @IBOutlet weak var setImage: UIImageView!
+    
+    @IBOutlet weak var settingsImageButton: UIButton!
+    
+    @IBOutlet weak var recipeButton: ButtonViews!
+    
+    @IBOutlet weak var directionsButton: ButtonViews!
+    
+    @IBOutlet weak var saveButton: ButtonViews!
+    
+    @IBOutlet weak var recipeTextField: UITextView!
+    
+    
+    @IBOutlet weak var directionsTextField: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func settingsImagePressed(_ sender: Any) {
+    }
+    
+    @IBAction func recipePressed(_ sender: Any) {
+    }
+    
+    @IBAction func directionsPress(_ sender: Any) {
+    }
+    
+    @IBAction func savePressed(_ sender: Any) {
+    }
+    
     /*
     // MARK: - Navigation
 
---
+++ MyRestaturant/Model/ButtonViews.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonViews.swift
+//  MyRestaturant
+//
+//  Created by Anthony Baldwin on 2/22/19.
+//  Copyright © 2019 AnthonyBaldwin. All rights reserved.
+//
+
+import UIKit
+
+class ButtonViews: UIButton {
+
+    override func awakeFromNib() {
+        layer.cornerRadius = 5
+    }
+
+}
